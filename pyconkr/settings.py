e7fd0abfe5da5e392409bd2f9aeac962cb4eb231
Parkseongheum <ab6449@gmail.com>
2021-08-14 13:34:24 +0900

    DB 설정 추가

--- pyconkr/settings.py
+++ pyconkr/settings.py
@@ -89,6 +89,15 @@ DATABASES = {
     }
 }
 
+if os.getenv('POSTGRES_NAME'):
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_NAME'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
