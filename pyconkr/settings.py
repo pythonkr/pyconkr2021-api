d45c42bb39ccbf9c5c8887f5ffa63d46f384b600
Parkseongheum <ab6449@gmail.com>
2021-08-19 23:59:18 +0900

    ALLOWED_HOST 추가

--- pyconkr/settings.py
+++ pyconkr/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-)$x@507chah=nr9do0a-z04z*5bc$j$a$$vcv7y#rt3bn=7)5b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dev-pycon-kr-2021-1145975631.ap-northeast-2.elb.amazonaws.com']
 
 # ELB Health check
 if 'ECS_CONTAINER_METADATA_URI_V4' in os.environ:
