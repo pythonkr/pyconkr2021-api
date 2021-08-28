5d93ec3fe53a27a7ed57704416f11d27fca416d5
Parkseongheum <ab6449@gmail.com>
2021-08-29 03:39:24 +0900

    공지사항 조회시 timezone(UTC+9) 적용

--- article/models.py
+++ article/models.py
@@ -7,3 +7,6 @@ class Article(models.Model):
     visible_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
--- article/viewsets.py
+++ article/viewsets.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, timezone, timedelta
 
 from django.db.models.functions import Coalesce
 from rest_framework.viewsets import ReadOnlyModelViewSet
@@ -7,6 +7,9 @@ from article.models import Article
 from article.serializers import ArticleSerializer
 
 
+KST = timezone(timedelta(hours=9))
+
+
 class ArticleViewSet(ReadOnlyModelViewSet):
-    queryset = Article.objects.filter(visible_at__lte=datetime.datetime.now()).order_by('-created_at')
+    queryset = Article.objects.filter(visible_at__lte=datetime.now(tz=KST)).order_by('-created_at')
     serializer_class = ArticleSerializer
