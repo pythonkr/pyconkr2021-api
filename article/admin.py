9e7ae305e6bf4f7a339cd9f6088bb3122cc29272
Parkseongheum <ab6449@gmail.com>
2021-08-29 03:52:25 +0900

    공지사항 Admin 등록

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from article.models import Article
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'visible_at',)
+
+
+admin.site.register(Article, ArticleAdmin)
