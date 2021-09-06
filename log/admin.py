92d4be4c4357879f793d77ca7d14ac09223e135e
Parkseongheum <ab6449@gmail.com>
2021-09-06 23:43:20 +0900

    Django Admin의 Action으로 로그 삭제 불가능하도록 해당 Action을 Override

--- log/admin.py
+++ log/admin.py
@@ -4,9 +4,13 @@ from log.models import Log
 
 
 class LogAdmin(admin.ModelAdmin):
-    readonly_fields = ('type', 'user', 'msg', 'ts',)
+    readonly_fields = ('type', 'user', 'msg', 'ts', 'ip')
     list_display = ['type', 'user', 'ip', 'ts']
     ordering = ('-ts',)
+
+    # Django Admin의 Action으로 삭제 불가능하도록 해당 Action을 Override
+    def delete_queryset(self, request, queryset):
+        pass
 
 
 admin.site.register(Log, LogAdmin)
