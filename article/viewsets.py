68877164a98f144f2cda3d954fece8ed1e95d122
Parkseongheum <ab6449@gmail.com>
2021-09-07 22:11:47 +0900

    datetime 객체의 동적생성을 위해 get_queryset override (클래즈에 명시된 queryset은 무시됨)

--- article/viewsets.py
+++ article/viewsets.py
@@ -7,9 +7,13 @@ from article.models import Article
 from article.serializers import ArticleSerializer
 
 
-KST = timezone(timedelta(hours=9))
+KST = timezone(timedelta(hours=0))
 
 
 class ArticleViewSet(ReadOnlyModelViewSet):
     queryset = Article.objects.filter(visible_at__lte=datetime.now(tz=KST)).order_by('-created_at')
     serializer_class = ArticleSerializer
+
+    # datetime 객체의 동적생성을 위해 get_queryset override (상단의 queryset은 무시됨)
+    def get_queryset(self):
+        return Article.objects.filter(visible_at__lte=datetime.now(tz=KST)).order_by('-created_at')
