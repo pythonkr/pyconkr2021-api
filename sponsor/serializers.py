fa13f75f1685d8cc22277c13c41e3033d440fa7a
Parkseongheum <ab6449@gmail.com>
2021-09-27 01:11:07 +0900

    Revert "Merge branch 'develop'"
    This reverts commit 8b9f92aa45c7b7a6cb54ba647a721fb6c2e66f9d.

--- sponsor/serializers.py
+++ sponsor/serializers.py
@@ -8,7 +8,7 @@ from sponsor import models
 class SponsorSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Sponsor
-        fields = ['name', 'desc', 'logo_image', 'url', 'slug', 'virtual_booth_content']
+        fields = ['name', 'desc', 'logo_image', 'url', 'slug']
 
 
 class SponsorLevelSerializer(serializers.ModelSerializer):
