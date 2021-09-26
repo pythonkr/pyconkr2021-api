4126f00a5d8215d98c6b31964f608a617a5133cc
Parkseongheum <ab6449@gmail.com>
2021-09-26 22:11:54 +0900

    후원사 API 필드 추가

--- sponsor/serializers.py
+++ sponsor/serializers.py
@@ -8,7 +8,7 @@ from sponsor import models
 class SponsorSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Sponsor
-        fields = ['name', 'desc', 'logo_image', 'slug']
+        fields = ['name', 'desc', 'logo_image', 'url', 'slug']
 
 
 class SponsorLevelSerializer(serializers.ModelSerializer):
