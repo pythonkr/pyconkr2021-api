902776df35e54ce0958861e9797e48e92331d684
Parkseongheum <ab6449@gmail.com>
2021-09-30 21:37:35 +0900

    category 필드 수정

--- program/serializers.py
+++ program/serializers.py
@@ -3,6 +3,12 @@ from rest_framework import serializers
 from program.models import Proposal, ProgramCategory
 
 
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProgramCategory
+        fields = ['id', 'name']
+
+
 class ProposalSerializer(serializers.ModelSerializer):
     video_open_at = serializers.DateTimeField(format='%m/%d %H:%M')
     difficulty = serializers.ChoiceField(choices=(
@@ -15,10 +21,15 @@ class ProposalSerializer(serializers.ModelSerializer):
                                     ('L', '30min'),
                                 ))
     category = serializers.StringRelatedField()
+    category_data = serializers.SerializerMethodField()
+
+    def get_category_data(self, obj):
+        return CategorySerializer(obj.category).data
 
     class Meta:
         model = Proposal
         fields = ['title', 'brief', 'desc', 'comment', 'difficulty',
                   'duration', 'language', 'category', 'introduction', 'video_url',
                   'slide_url', 'video_open_at', 'user_name', 'id',
+                  'category_data'
                   ]
